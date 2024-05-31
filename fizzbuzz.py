d7feec8bd97545bc6f71d7649b36b13f115e167d
hs0517 <105457333+hs0517@users.noreply.github.com>
2024-05-31 14:23:58 +0900

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,8 @@
 def fizzbuzz(num):
-  if num % 3 == 0 and num % 5 == 0:
-    return 'FizzBuzz'
-  if num % 3 == 0:
-    return 'Fizz'
-  if num % 5 == 0:
-    return 'Buzz'
-  return num
+    if num % 3 == 0 and num % 5 == 0:
+      return 'FizzBuzz'
+    if num % 3 == 0:
+      return 'Fizz'
+    if num % 5 == 0:
+      return 'Buzz'
+    return num
